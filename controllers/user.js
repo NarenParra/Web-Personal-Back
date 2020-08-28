2dece9f0f35b4bb9d3917eeb593fbb15faa36a95
Naren Parra <naren21p@gmail.com>
2020-08-27 21:34:15 -0500

    Model User

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,43 @@
 const bcrypt = require("bcrypt-nodejs");
-const user = require("../models/user");
+const User = require("../models/user");
 
 function singUp(req, res) {
-  console.log("End point  singUp");
+  const user = new User();
+
+  const { name, lastName, email, password, repeatPassword } = req.body;
+  user.name = name;
+  user.lastName = lastName;
+  user.email = email.toLowerCase();
+  user.role = "admin";
+  user.active = false;
+
+  if (!password || !repeatPassword) {
+    res.status(404).send({ message: "Las contrasenias son obligatorias" });
+  } else {
+    if (password !== repeatPassword) {
+      res.status(404).send({ message: "Las contrasenias deben ser iguales" });
+    } else {
+      bcrypt.hash(password, null, null, function (err, hash) {
+        if (err) {
+          res.status(500).send({ message: "error al encriptar contrasenia" });
+        } else {
+          user.password = hash;
+
+          user.save((err, userStored) => {
+            if (err) {
+              res.status(500).send({ message: "Error, el usuario ya existe " });
+            } else {
+              if (!userStored) {
+                res.status(404).send({ message: "error al creact usuario" });
+              } else {
+                res.status(200).send({ user: userStored });
+              }
+            }
+          });
+        }
+      });
+    }
+  }
 }
 
 module.exports = {
